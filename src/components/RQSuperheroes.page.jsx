19f67066d4193f5b5efb70711b8e88b42a2496d1
Shivananda Sai <shivanandasai.38@gmail.com>
2024-07-31 18:17:36 +0530

    chapter 13 checkpoint

--- src/components/RQSuperheroes.page.jsx
+++ src/components/RQSuperheroes.page.jsx
@@ -1,25 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-
-const fetchSuperheroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
-};
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export default function RQSuperheroesPage() {
-  const { data, isLoading, isError, error, refetch } = useQuery({
-    queryKey: ["super-heroes"], // This means that the query will be cached with the key super-heroes
-    queryFn: () => fetchSuperheroes(), // this means that the query will be executed by calling the fetchSuperheroes function
-    // gcTime: 5000, // Garbage collection time, this means that the cache will be cleared after 5 seconds
-    // staleTime: 30000, // this means that the cache will be considered stale after 30 seconds
-    // refetchOnMount: true // This means that the data will be refetched when the component mounts
-    // refetchOnWindowFocus: true // This means that the data will be refetched when the window is in focus
-    // refetchInterval: 2000, // This means that the data will be refetched every 2 seconds. It is paused when the window is not in focus
-    // refetchIntervalInBackground: true, // This means that the data will be refetched every 2 seconds even when the window is not in focus
-    // enabled: false, // This means that the query will not be executed when the component mounts
-    // select: (data) =>
-    //   data.data.map((superhero) => superhero.name.toUpperCase()), // This means that the data will be transformed before it is returned
-    // // Follow this docs for better code practices: https://tkdodo.eu/blog/react-query-data-transformations#3-using-the-select-option
-  });
+  const { data, isLoading, isError, error, refetch } = useSuperHeroesData();
 
   if (isError) return <div> Error: {error.message} </div>;
 
