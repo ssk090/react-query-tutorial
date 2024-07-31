ea7756d1ed7a301a8a79a2eaa7ab98e7a1685a09
Shivananda Sai <shivanandasai.38@gmail.com>
2024-07-31 18:06:12 +0530

    chapter 12 checkpoint

--- src/components/RQSuperheroes.page.jsx
+++ src/components/RQSuperheroes.page.jsx
@@ -7,17 +7,19 @@ const fetchSuperheroes = () => {
 
 export default function RQSuperheroesPage() {
   const { data, isLoading, isError, error, refetch } = useQuery({
-    queryKey: ["super-heroes"],
-    queryFn: () => fetchSuperheroes(),
+    queryKey: ["super-heroes"], // This means that the query will be cached with the key super-heroes
+    queryFn: () => fetchSuperheroes(), // this means that the query will be executed by calling the fetchSuperheroes function
     // gcTime: 5000, // Garbage collection time, this means that the cache will be cleared after 5 seconds
     // staleTime: 30000, // this means that the cache will be considered stale after 30 seconds
     // refetchOnMount: true // This means that the data will be refetched when the component mounts
     // refetchOnWindowFocus: true // This means that the data will be refetched when the window is in focus
     // refetchInterval: 2000, // This means that the data will be refetched every 2 seconds. It is paused when the window is not in focus
     // refetchIntervalInBackground: true, // This means that the data will be refetched every 2 seconds even when the window is not in focus
-    enabled: false, // This means that the query will not be executed when the component mounts
+    // enabled: false, // This means that the query will not be executed when the component mounts
+    // select: (data) =>
+    //   data.data.map((superhero) => superhero.name.toUpperCase()), // This means that the data will be transformed before it is returned
+    // // Follow this docs for better code practices: https://tkdodo.eu/blog/react-query-data-transformations#3-using-the-select-option
   });
-  console.log(data);
 
   if (isError) return <div> Error: {error.message} </div>;
 
@@ -27,7 +29,7 @@ export default function RQSuperheroesPage() {
     <div>
       <h1>RQSuperheroesPage</h1>
       {/*  useQuery on Click of button */}
-      {/* <button onClick={refetch}>Fetch Superheroes</button> */}
+      <button onClick={refetch}>Fetch Superheroes</button>
       {data?.data.map((superhero) => {
         return (
           <div key={superhero.id}>
@@ -36,6 +38,14 @@ export default function RQSuperheroesPage() {
           </div>
         );
       })}
+
+      {/* {data.map((superhero) => {
+        return (
+          <div key={superhero}>
+            <h2>{superhero}</h2>
+          </div>
+        );
+      })} */}
     </div>
   );
 }
